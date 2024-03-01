5a09f4754096a03f60fa0e986e3fe02c10461e16
Николай Полозов <kolya.polozov@inbox.ru>
2024-03-01 11:19:30 +0500

    registration released

---
+++ app/src/main/java/com/example/hospitalg/ConnDB.java
@@ -0,0 +1,64 @@
+package com.example.hospitalg;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnDB extends SQLiteOpenHelper {
+
+    public ConnDB(Context context){
+        super(context, "hospitalDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("logs", "create user table");
+
+        db.execSQL("create table users(" +
+                "id integer primary key autoincrement," +
+                "login text," +
+                "password text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+//    private static final String LOG = "DEBUG";
+//    private static String ip = "127.0.0.1";
+//    //private static String ip = "HOME-PC\\Николай";
+//    private static String port = "1433";
+//    private static String classs = "net.sourceforge.jtds.jdbc.Driver";
+//    private static String db = "hospitalDB";
+//    private static String un = "user";
+//    private static String password = "user";
+//    public static Connection connection() {
+//        Connection conn = null;
+//        String ConnURL = null;
+//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//        StrictMode.setThreadPolicy(policy);
+//        try {
+//            Class.forName(classs);
+//            ConnURL = "jdbc:jtds:sqlserver://" + ip +":"+port+";"
+//                    + "databaseName=" + db + ";user=" + un + ";password="
+//                    + password + ";";
+//            //ConnURL = "jdbc:jtds:sqlserver://" + ip + ";databaseName" + db + ";integratedSecurity=true";
+//            conn = DriverManager.getConnection(ConnURL);
+//        } catch (SQLException e) {
+//            Log.d(LOG, e.getMessage());
+//        } catch (ClassNotFoundException e) {
+//            Log.d(LOG, e.getMessage());
+//        }
+//        return conn;
+//    }
+
+
+
+}
--- app/src/main/java/com/example/hospitalg/MainActivity.java
+++ app/src/main/java/com/example/hospitalg/MainActivity.java
@@ -1,14 +1,95 @@
 package com.example.hospitalg;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
+    Connection connection;
+    ConnDB conn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        conn = new ConnDB(this);
     }
+
+    @SuppressLint("Range")
+    public void registration(View view){
+
+        String login = ((EditText) findViewById(R.id.editTextUsername)).getText().toString();
+        String password = ((EditText) findViewById(R.id.editTextPassword)).getText().toString();
+        String passConf = ((EditText) findViewById(R.id.editTextConfirmPassword)).getText().toString();
+
+        if(password.equals(passConf)){
+            SQLiteDatabase db = conn.getWritableDatabase();
+
+            Cursor c = db.query("users", null, "login =" + login, null, null, null, null);
+
+            if(c.moveToFirst()){
+
+//                int idColIndex = c.getColumnIndex("id");
+//                int loginColIndex = c.getColumnIndex("login");
+//                int passColIndex = c.getColumnIndex("password");
+//
+//                do{
+//                    Log.d("Message", "ID = " + c.getInt(idColIndex) +
+//                            ", login = " + c.getString(loginColIndex) +
+//                            ", password = " + c.getString(passColIndex));
+//                } while (c.moveToNext());
+                AlertDialog("Регистрация", "Юзер с таким же логином уже находится в бд!!");
+                return;
+            }
+
+            Log.d("Message", "Insert in table");
+
+            ContentValues cv = new ContentValues();
+            cv.put("login", login);
+            cv.put("password", password);
+            long rowID = db.insert("users", null, cv);
+            Log.d("Message", "row inserted, ID = " + rowID);
+            AlertDialog("Регистрация", "Вы были зарегистрированы!");
+            return;
+        }
+        AlertDialog("Регистрация", "Пароли не совпадают!");
+    }
+
+    public void AlertDialog(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void onAuthorization(View view){
+        setContentView(R.layout.login);
+    }
+
+
 }
